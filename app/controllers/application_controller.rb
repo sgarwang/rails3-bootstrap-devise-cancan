0ad21974201a441b858713652dfe8acdb8d19579
sgar.wang <sgar.wang@splashtop.com>
2013-05-09 14:27:31 +0800

    Change: [CH9] first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
+
+  # https://github.com/plataformatec/devise/wiki/How-To%3A-Redirect-to-a-specific-page-on-successful-sign-in-out
+  # def after_sign_in_path_for(resource)
+  #  current_user_path
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails3BootstrapDeviseCancan::Application.routes.draw do
+  get "home/index"
+
+  authenticated :user do
+    root :to => 'home#index'
+  end
+
+  root :to => "home#index"
+  
   devise_for :users
 
   # The priority is based upon order of creation:
