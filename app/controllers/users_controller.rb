c4db71d8938a0e3ec484bf1a5049353c84ba2fb4
sgar.wang <sgar.wang@splashtop.com>
2013-05-09 15:36:58 +0800

    Change: [ch13] initial add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,44 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
+  # load_and_authorize_resource :only => :index
+  # before_filter :only_allow_admin, :only => [ :index ]
+
   def index
+    authorize! :index, @user, :message => 'Not authorized as an administrator [cc_c_m].'
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  def update
+    authorize! :update, @user, :message => 'Not authorized as an administrator. [cc_c_m]'
+    @user = User.find(params[:id])
+
+    # Let the User model know that we want to override the mass-assignment restrictions set by the attr_accessible statement in the User model.
+    if @user.update_attributes(params[:user], :as => :admin)
+      redirect_to users_path, :notice => "User updated."
+    else
+      redirect_to users_path, :alert => "Unable to update user."
+    end
+  end
+
+  def destroy
+    authorize! :destroy, @user, :message => 'Not authorized as an administrator.'
+    user = User.find(params[:id])
+    unless user == current_user
+      user.destroy
+      redirect_to users_path, :notice => "User deleted."
+    else
+      redirect_to users_path, :notice => "Can't delete yourself."
+    end
+  end
+
+  private
+    def only_allow_admin
+      redirect_to root_path, :alert => 'Not authorized as an administrator [rails].' unless
+      current_user.has_role? :admin
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
+  attr_accessible :role_ids, :as => :admin
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 
   validates_presence_of :name
-  validates_uniqueness_of :name, :email, :case_sensitive => false
+  validates_uniqueness_of :email, :case_sensitive => false
 end
